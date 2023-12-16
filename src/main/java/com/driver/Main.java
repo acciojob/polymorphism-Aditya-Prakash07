1fe63a5511f77e775d011c0fa4ca2a617824e5a9
Aditya <prakashaditya775@gmail.com>
2023-12-16 22:34:42 +0530

    code change for polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+     public static class Product {
+        public int product(int a, int b) {
+            return a * b;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(3,7));
+        System.out.println(p.product(4,5,6));
+        System.out.println(p.product(3.4,6.5));
+    }
 }
